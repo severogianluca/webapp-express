2dc189459abf9ab5268e024d640ede8bf0e748f8
severogianluca <gianlucasevero300@gmail.com>
2025-05-10 04:23:01 +0200

    add: set port on app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log("ciaooooo")
+const express = require('express')
+const app = express()
+const port = 4000
+
+app.get('/', (req, res) => {
+    res.send('Welcome in our homepage')
+})
+
+app.listen(port, () =>{
+    console.log(`web-app in ascolto nella porta ${port}`)
+})
