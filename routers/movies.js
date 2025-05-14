5c11ce1a4c166a9587dab50abbf9f81d1740cbaf
severogianluca <gianlucasevero300@gmail.com>
2025-05-14 16:58:33 +0200

    add route post reviews

--- routers/movies.js
+++ routers/movies.js
@@ -5,5 +5,6 @@ const movieController = require('../controllers/moviesController');
 // Rotte definite con il controller
 router.get('/', movieController.getList);
 router.get('/:id', movieController.getById);
+router.post('/:id/reviews', movieController.insertReviews);
 
 module.exports = router;
